631d7cde3a4294a3a74c131603c4b2a611ae8076
Alejandro Marquez <alejandromarquez@live.com>
2021-01-16 20:13:34 -0500

    ( Alejandro M ) First Commit

---
+++ routine/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+#class Food(models.Model):
+#    exp_date = models.DateField(auto_now=False, auto_now_add=True)
+#    purchase_date = models.DateField(auto_now=True, auto_now_add=True)
+#    longetivity = models.DurationField()
+#    quantity
+#    cost
+#    calories
