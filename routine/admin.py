8f6f7e06625c55b572d4b9e1da5935977e2162b0
Alo Marquez <alejandromarquez@live.com>
2021-02-11 02:15:13 -0500

    ( Alejandro M. ) Made SQLite for site, want to use MySQL

--- routine/admin.py
+++ routine/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from .models import Refrigerator, Item
 # Register your models here.
+
+admin.site.register(Refrigerator)
+admin.site.register(Item)
--- routine/models.py
+++ routine/models.py
@@ -1,10 +1,28 @@
 from django.db import models
 
 # Create your models here.
-#class Food(models.Model):
-#    exp_date = models.DateField(auto_now=False, auto_now_add=True)
-#    purchase_date = models.DateField(auto_now=True, auto_now_add=True)
-#    longetivity = models.DurationField()
-#    quantity
-#    cost
-#    calories
+class Refrigerator(models.Model):
+    #exp_date = models.DateField(auto_now=False, auto_now_add=True)
+    #purchase_date = models.DateField(auto_now=True, auto_now_add=True)
+    #longetivity = models.DurationField()
+    item_id         = models.PositiveIntegerField()
+    user_id         = models.PositiveIntegerField()
+    purchase_date   = models.DateField(auto_now_add=True)
+    quantity        = models.PositiveSmallIntegerField()
+
+class Item(models.Model):
+    name        = models.TextField()
+    longetivity = models.DurationField()
+    cost        = models.FloatField()
+    calories    = models.SmallIntegerField()
+    TYPE_ITEM   = [
+        ('FD', 'Food'),
+        ('DV', 'Device'),
+        ('HH', 'Household Object'),
+        ('CL', 'Clothing'),
+        ('MC', 'Misc'),
+    ]
+    type        = models.CharField(max_length=30, choices=TYPE_ITEM)
+
+    def __str__(self):
+        return self.name[:15]
